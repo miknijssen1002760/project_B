9d92c7d0a6e5c838c0b0c96a8951e6cb8c6d5351
Sem Hirschbein <1030180@hr.nl>
2022-03-21 14:19:44 +0100

    basic menu

---
+++ project_B/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace project_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====account====");
+            Console.WriteLine("\r1. Sign in");
+            Console.WriteLine("\r2. register");
+            Console.Write("\nChoose an option: ");
+
+            bool run = true;
+            while (run == true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        run = false;
+                        Console.WriteLine("dummy sign in text\n");
+                        break;
+
+                    case "2":
+                        run = false;
+                        Console.WriteLine("dummy register text\n");
+                        break;
+
+                    default:
+                        Console.Write("This is not a valid option please try again: ");
+                        break;
+                }
+            }
+            Console.ReadLine(); 
+            Console.Clear();
+            Console.WriteLine("====menu====");
+            Console.WriteLine("1. beschikbare vluchten");
+            Console.WriteLine("2. vlucht boeken");
+            Console.WriteLine("3. geboekte vluchten");
+            Console.Write("\nChoose an option: ");
+
+            run = true;
+            while (run == true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        run = false;
+                        Console.WriteLine("insert beschikbare vluchten\n");
+                        break;
+
+                    case "2":
+                        run = false;
+                        Console.WriteLine("insert vlucht boeken\n");
+                        break;
+
+                    case "3":
+                        run = false;
+                        Console.WriteLine("insert geboekte vluchten");
+                        break;
+
+                    default:
+                        Console.WriteLine("This is not a valid option please try again: ");
+                        break;
+
+                }
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
