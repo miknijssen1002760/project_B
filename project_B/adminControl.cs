2365a217e2087cf8fc5ef7213ec3e4eb99108f0d
JesseVDHulle <jessevanderhulle@gmail.com>
2022-03-21 12:40:08 +0100

    Menu toevoegingen en lege functies

--- project_B/adminControl.cs
+++ project_B/adminControl.cs
@@ -4,6 +4,16 @@ namespace AirlineFood
 {
     internal class Program
     {
+        public static string[][] strToLayout(string layoutStr)
+        {
+            string[][] StrSPlit = new string[3][];//COnvert string to string[][]
+            return StrSPlit;
+        }
+        public static int genID()
+        {
+            int current = 0;//get last ID
+            return current + 1;
+        }
         public static void addPlane()
         {
             Console.WriteLine("Voeg Vliegtuig toe aan Systeem:");
@@ -18,42 +28,43 @@ namespace AirlineFood
             Console.WriteLine("\n" + $"ID assigned: {ID}");
 
             addPlaneToFile(name, ID, layout);
-
-        }
-        public static string[][] strToLayout(string layoutStr)
-        {
-            string[][] StrSPlit = new string[3][];//COnvert string to string[][]
-            return StrSPlit;
-        }
-        public static int genID()
-        {
-            int current = 0;//get last ID
-            return current + 1;
         }
         public static void delPlane()
         {
 
         }
+        public static void changePlaneName() 
+        { 
+        
+        }
+        public static void changePlaneLayout() 
+        { 
+            
+        }
         public static void addPlaneToFile(string name, int planeID, string[][] layout)
         {
             Console.WriteLine("\n" + $"Name: {name}, PlaneID: {planeID}, Layout: {layout}");
+
         }
         public static void delPlaneToFile(int planeID)
         {
 
         }
-        public static void changePlaneName(int planeID, string name)
+        public static void changePlaneNameToFile(int planeID, string name)
         {
 
         }
-        public static void changePlaneLayout(int planeID, string[][] layout)
+        public static void changePlaneLayoutToFile(int planeID, string[][] layout)
         {
 
         }
+
+
+
 
         public static string chooseOption()
         {
-            Console.WriteLine("\nVoer in Getal: ");
+            Console.Write("\nVoer in Getal: ");
             string? chosen = Console.ReadLine();
             Console.WriteLine("--------------------------------------------------------");
 
@@ -62,9 +73,9 @@ namespace AirlineFood
         }
         public static string chooseMainOption()
         {
-            Console.WriteLine("\nWat wil je beheren?:");
+            Console.WriteLine("Wat wil je beheren?:");
             Console.WriteLine("\t1. Vliegtuigen");
-            Console.WriteLine("\t2. members");
+            Console.WriteLine("\t2. Gebruikers");
             Console.WriteLine("\t3. Vluchten");
             
             string chosen = chooseOption();
@@ -72,9 +83,11 @@ namespace AirlineFood
         }
         public static void choosePlaneOption()
         {
-            Console.WriteLine("\nKies Optie: (getal)");
+            Console.WriteLine("====VliegtuigBeheer====");
             Console.WriteLine("\t1. Voeg vliegtuig toe");
             Console.WriteLine("\t2. Verwijder Vliegtuig ");
+            Console.WriteLine("\t3. Wijzig Naam Vliegtuig");
+            Console.WriteLine("\t4. Wijzig Stoellayout Vliegtuig");
 
             string chosen = chooseOption();
 
@@ -82,7 +95,61 @@ namespace AirlineFood
                 addPlane();
             } else if (chosen == "2") {
                 delPlane();
+            } else if (chosen == "3") {
+                changePlaneName();
+            } else if (chosen == "4") {
+                changePlaneLayout();
             } else {
+                Console.WriteLine("Verkeerde Input");
+            }
+        }
+        public static void chooseUserOption()
+        {
+            Console.WriteLine("====GebruikerBeheer====");
+            Console.WriteLine("\t1. Voeg gebruiker toe");
+            Console.WriteLine("\t2. Verwijder gebruiker");
+            Console.WriteLine("\t3. Wijzig gebruiker");
+
+            string chosen = chooseOption();
+
+            if (chosen == "1")
+            {
+                //tbm
+            }
+            else if (chosen == "2")
+            {
+                //tbm
+            }
+            else if (chosen == "3")
+            {
+                //tbm
+            } else { 
+                Console.WriteLine("Verkeerde Input");
+            }
+        }
+        public static void chooseFlightOption() 
+        {
+            Console.WriteLine("====VluchtBeheer====");
+            Console.WriteLine("\t1. Voeg Vlucht toe");
+            Console.WriteLine("\t2. Verwijder Vlucht");
+            Console.WriteLine("\t3. Wijzig Vlucht");
+
+            string chosen = chooseOption();
+
+            if (chosen == "1")
+            {
+                //tbm
+            }
+            else if (chosen == "2")
+            {
+                //tbm
+            }
+            else if (chosen == "3")
+            {
+                //tbm
+            }
+            else
+            {
                 Console.WriteLine("Verkeerde Input");
             }
         }
@@ -93,9 +160,9 @@ namespace AirlineFood
             if (chosenMain == "1") {
                 choosePlaneOption();
             } else if (chosenMain == "2") {
-                //tbm
+                chooseUserOption();
             } else if (chosenMain == "3") {
-                //tbm
+                chooseFlightOption();
             } else {
                 Console.WriteLine("Verkeerde Input");
             }
