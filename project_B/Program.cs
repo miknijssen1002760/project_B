e152d155483da92ada2521c30ddbbac683070c01
miknijssen1002760 <1002760@hr.nl>
2022-03-28 15:09:57 +0200

    GetDestination uit vlucht_boeken brache gekopieerd

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -5,14 +5,21 @@ namespace project_B
 {
     internal class Program
     {
+        static string GetDestination()
+        {
+            string Destination;
+            Console.WriteLine("waar gaat de reis naartoe?");
+            Destination = Console.ReadLine();
+            Console.WriteLine($"Bestemming: {Destination}");
+            return Destination;
+        }
         static void Main(string[] args)
         {
             Flights flightController = new Flights();
-            /*Flight currentFlight = flightController.getId(1);
+            Flight currentFlight = flightController.getId(1);
             currentFlight.Destination = "Tokyo";
-            currentFlight.writeToFile();*/
-            flightController.displayFlights("hiroshima");
-            //flightController.displayFlights(Console.ReadLine());
+            currentFlight.writeToFile();
+            flightController.displayFlights(GetDestination());
         }
     }
 }
