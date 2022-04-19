a0e4c16eeeb48f1e7bce990ae1fcc995caff0b99
miknijssen1002760 <1002760@hr.nl>
2022-04-19 13:13:07 +0200

    Revert "get" you know what never mind This reverts commit 125fa9206f9a1e4afd49fca271ba8436f2ed7fad.

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -5,14 +5,22 @@ namespace project_B
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static string GetDestination()
         {
-            Flights flightController = new Flights();
             string Destination;
             Console.Write("waar gaat de reis naartoe? ");
             Destination = Console.ReadLine();
+            Console.Clear();
             Console.WriteLine($"Bestemming: {Destination}");
-            Console.WriteLine(flightController.GetFlights(Destination));
+            return Destination;
+        }
+        static void Main(string[] args)
+        {
+            Flights flightController = new Flights();
+            Flight currentFlight = flightController.getId(2);
+            currentFlight.Destination = "London";
+            currentFlight.writeToFile();
+            Console.Write(flightController.GetFlights(GetDestination()));
         }
     }
 }
