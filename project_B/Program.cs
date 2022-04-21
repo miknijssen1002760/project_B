6a2d55c6eec310081d37cce5c190233b4116e3e4
miknijssen1002760 <1002760@hr.nl>
2022-04-21 23:19:44 +0200

    idk what happend

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -5,22 +5,18 @@ namespace project_B
 {
     public class Program
     {
-        public static string GetDestination()
-        {
-            string Destination;
-            Console.Write("waar gaat de reis naartoe? ");
-            Destination = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine($"Bestemming: {Destination}");
-            return Destination;
-        }
         static void Main(string[] args)
         {
             Flights flightController = new Flights();
             Flight currentFlight = flightController.getId(2);
             currentFlight.Destination = "London";
             currentFlight.writeToFile();
-            Console.Write(flightController.GetFlights(GetDestination()));
+            string Destination;
+            Console.Write("waar gaat de reis naartoe? ");
+            Destination = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Bestemming: {Destination}");
+            Console.Write(flightController.GetFlights(Destination));
         }
     }
 }
