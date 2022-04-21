5206dba9ec46252173a6246761ac589ee86591f6
miknijssen1002760 <1002760@hr.nl>
2022-04-21 23:35:40 +0200

    edit the destination using this

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -8,6 +8,9 @@ namespace project_B
         static void Main(string[] args)
         {
             Flights flightController = new Flights();
+            //Flight currentFlight = flightController.getId(4);
+            //currentFlight.Destination = "New York";
+            //currentFlight.writeToFile();
             string Destination;
             Console.Write("waar gaat de reis naartoe? ");
             Destination = Console.ReadLine();
