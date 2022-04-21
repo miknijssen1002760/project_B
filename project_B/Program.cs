b366d8835546b09d7e8af70c0da7ddb8a9c32ca1
miknijssen1002760 <1002760@hr.nl>
2022-04-21 23:45:27 +0200

    ik heb er toch maar een functie van gemaakt.

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -5,12 +5,26 @@ namespace project_B
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void EditDestination(string dest1, string dest2, string dest3, string dest4)
         {
             Flights flightController = new Flights();
-            //Flight currentFlight = flightController.getId(4);
-            //currentFlight.Destination = "New York";
-            //currentFlight.writeToFile();
+            Flight currentFlight = flightController.getId(1);
+            currentFlight.Destination = dest1;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(2);
+            currentFlight.Destination = dest2;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(3);
+            currentFlight.Destination = dest3;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(4);
+            currentFlight.Destination = dest4;
+            currentFlight.writeToFile();
+        }
+        static void Main(string[] args)
+        {
+            EditDestination("Tokyo", "London", "New York", "Tokyo");
+            Flights flightController = new Flights();
             string Destination;
             Console.Write("waar gaat de reis naartoe? ");
             Destination = Console.ReadLine();
