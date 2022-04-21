83f319eb48895d643839abb1eb0624bc26b2be85
miknijssen1002760 <1002760@hr.nl>
2022-04-21 23:28:02 +0200

    no i know what happend and its fixed

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -8,15 +8,11 @@ namespace project_B
         static void Main(string[] args)
         {
             Flights flightController = new Flights();
-            Flight currentFlight = flightController.getId(2);
-            currentFlight.Destination = "London";
-            currentFlight.writeToFile();
             string Destination;
             Console.Write("waar gaat de reis naartoe? ");
             Destination = Console.ReadLine();
-            Console.Clear();
             Console.WriteLine($"Bestemming: {Destination}");
-            Console.Write(flightController.GetFlights(Destination));
+            Console.WriteLine(flightController.GetFlights(Destination));
         }
     }
 }
