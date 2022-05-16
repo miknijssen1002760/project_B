b06fc5881d6454c715189baa2e428865e88c372e
miknijssen1002760 <1002760@hr.nl>
2022-05-16 15:33:11 +0200

    alles verplaatst van consoleApp1 project_B

---
+++ project_B/Controllers/Flights.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Controllers
+{
+    internal class Flights
+    {
+        private List<Flight> _flights;
+        string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"Data/flights.json"));
+
+
+        public Flights()
+        {
+            Load();
+        }
+
+        public void Load()
+        {
+            string json = File.ReadAllText(path);
+
+            _flights = JsonSerializer.Deserialize<List<Flight>>(json);
+        }
+    }
+}
