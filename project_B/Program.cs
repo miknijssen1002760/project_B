dbf4ede0e9e15f1af8f7ccf4524e64397e10dbdf
Sem Hirschbein <1030180@hr.nl>
2022-05-22 22:25:01 +0200

    i hate my life

--- project_B/Program.cs
+++ project_B/Program.cs
@@ -1,4 +1,5 @@
 ﻿using project_B.Views;
+using project_B.Controllers;
 using System;
 
 namespace project_B
@@ -13,90 +14,15 @@ namespace project_B
 
         }
 
-        public static int MultipleChoice(bool canCancel, string CurrentMenu, params string[] options)
-        {
-            const int startX = 0;
-            const int startY = 1;
-            const int optionsPerLine = 1;
-            const int spacingPerLine = 14;
-            int currentSelection = 0;
-
-            ConsoleKey key;
-            Console.CursorVisible = false;
-
-            do
-            {
-                Console.Clear();
-
-                Console.Write(CurrentMenu);
-
-                for (int i = 0; i < options.Length; i++)
-                {
-                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
-
-                    if (i == currentSelection)
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.Write(options[i]);
-
-                    Console.ResetColor();
-                }
-
-                key = Console.ReadKey(true).Key;
-
-                switch (key)
-                {
-                    case ConsoleKey.UpArrow:
-                        {
-                            if (currentSelection >= optionsPerLine)
-                                currentSelection -= optionsPerLine;
-                            break;
-                        }
-
-                    case ConsoleKey.DownArrow:
-                        {
-                            if (currentSelection + optionsPerLine < options.Length)
-                                currentSelection += optionsPerLine;
-                            break;
-                        }
-
-                    case ConsoleKey.LeftArrow:
-                        {
-                            if (currentSelection % optionsPerLine > 0)
-                                currentSelection--;
-                            break;
-                        }
-
-                    case ConsoleKey.RightArrow:
-                        {
-                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
-                                currentSelection++;
-                            break;
-                        }
-
-                    case ConsoleKey.Escape:
-                        {
-                            if (canCancel)
-                                return -1;
-                            break;
-                        }
-                }
-            } while (key != ConsoleKey.Enter);
-
-            Console.CursorVisible = true;
-
-            return currentSelection;
-        }
-
         public static void HomeScreen()
         {
             string[] LoginMenu = { "Login", "Register", "Exit" };
-            int CurrentSelection = MultipleChoice(true, "===Account===", LoginMenu);
+            int CurrentSelection = MenuCreator.MultipleChoice(true, "===Account===", LoginMenu);
 
             switch (CurrentSelection)
             {
                 case 0:
-                    Login.Show();
+                    Login.LoginTest();
                     break;
                 case 1:
                     Registreren.Show();
@@ -111,7 +37,7 @@ namespace project_B
         public static void FlightMenu()
         {
             string[] FlightMenu = { "Beschikbare vluchten", "Vlucht Boeken", "Geboekte Vluchten", "Exit" };
-            int CurrentSelection = MultipleChoice(true, "===Menu===", FlightMenu);
+            int CurrentSelection = MenuCreator.MultipleChoice(true, "===Menu===", FlightMenu);
 
             switch (CurrentSelection)
             {
--- project_B/Views/Login.cs
+++ project_B/Views/Login.cs
@@ -1,20 +1,59 @@
-﻿using System;
+﻿using project_B.Controllers;
+using project_B.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace project_B.Views
 {
-    static class Login
+    public class Login
     {
-
-        public static void Show()
+        static void EditDestination(string dest1, string dest2, string dest3, string dest4)
         {
-            Console.Clear();
-            Console.WriteLine("Dummy text login\n");
-            Console.WriteLine("press enter to continue");
-            Console.ReadLine();
+            Flights flightController = new Flights();
+            Flight currentFlight = flightController.getId(1);
+            currentFlight.Destination = dest1;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(2);
+            currentFlight.Destination = dest2;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(3);
+            currentFlight.Destination = dest3;
+            currentFlight.writeToFile();
+            currentFlight = flightController.getId(4);
+            currentFlight.Destination = dest4;
+            currentFlight.writeToFile();
         }
+        static void DisplayList(List<Flight> list)
+        {
+            foreach (Flight flight in list)
+            {
+                Console.WriteLine(flight.Date);
+            }
+        }
+        public static void LoginTest()
+        {
+            EditDestination("London", "London", "New York", "Tokyo");
+            Users accounts = new Users();
+            Flights flightController = new Flights();
+            //Console.WriteLine(accounts.users);
+            User MainUser = accounts.Login(accounts);
+            if (MainUser == null)
+            {
+                Console.WriteLine("Try again later");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome {MainUser.UserName}, what would you like to do today?");
+            }
+            Flight currentFlight = flightController.getId(1);
+            accounts.Book(currentFlight, MainUser);
+            string Destination;
+            Console.Write("waar gaat de reis naartoe? ");
+            Destination = Console.ReadLine();
+            Console.WriteLine($"Bestemming: {Destination}");
+            DisplayList(flightController.GetFlights(Destination));
+        }
+
+
     }
 }
