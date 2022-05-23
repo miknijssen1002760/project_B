5cfa458565298e3077a61a9e631fa846a32f39b7
Jurgen Smits <1031111@hr.nl>
2022-05-23 11:00:03 +0200

    Geoff

--- project_B/Controllers/Users.cs
+++ project_B/Controllers/Users.cs
@@ -28,73 +28,75 @@ namespace Login.Controllers
             return users.Find(i => i.UserName == name);
         }
 
-        public User Create()
+        public User Create(string name, string pass, Users x)
         {
             User newUser = new User();
-            newUser.Id = i;
-            Console.WriteLine("Enter Username: ");
-            newUser.UserName = Console.ReadLine();
-            Console.WriteLine("Enter Password");
-            newUser.Password = Console.ReadLine();
-            i += 1;
-            users.Add(newUser);
-            Write();
-            return newUser;
-
+            newUser.Id = 3;
+            if (emailCheck(name, x))
+            {
+                newUser.UserName = name;
+                newUser.Password = pass;
+                users.Add(newUser);
+                Write();
+                return newUser;
+            }
+            else
+            {
+                return null;
+            }
         }
         public void Write()
         {
             File.WriteAllText(Path, JsonSerializer.Serialize<List<User>>(users));
         }
 
-        public bool PassCheck(string pass)
+        public bool PassCheck(string pass, string passAttempt)
         {
-            for (int i = 0; i < 3; i++)
+            if (passAttempt == pass)
             {
-                Console.WriteLine("Enter password");
-                string passAttempt = Console.ReadLine();
-                if (passAttempt == pass)
-                {
-                    return true;
-                }
-                else 
-                    Console.WriteLine("Wrong password");
-            }
-            Console.WriteLine("Too many wrong attempts");
-            return false;
-        }
-
-        public User Login(Users x)
-        {
-            Console.WriteLine("Username: ");
-            User CurrentUser = x.FindUser(Console.ReadLine());
-            if (CurrentUser == null)
-            {
-                Console.WriteLine("No account found, would you like to make one? [y/n]");
-                string ans = Console.ReadLine();
-                if (ans == "y")
-                {
-                    CurrentUser = Create();
-                    return CurrentUser;
-                }
-                else if (ans == "n")
-                {
-                    Login(x);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid response");
-                    Login(x);
-                }
-            }
-            if (x.PassCheck(CurrentUser.Password))
-            {
-                return CurrentUser;
+                return true;
             }
             else
             {
+                return false;
+            }
+        }
+
+        public User Login(Users x, string userName, string passWord)
+        {
+            User CurrentUser = x.FindUser(userName);
+            if (CurrentUser == null)
+            {
                 return null;
             }
+            else
+            {
+                if (x.PassCheck(CurrentUser.Password, passWord))
+                {
+                    return CurrentUser;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+        }
+
+        public bool emailCheck(string email, Users x)
+        {
+            return (email.Contains("@") && x.FindUser(email) == null);
+        }
+
+        public void remove(User user, Users x)
+        {
+            users.Remove(user);
+            Write();
+        }
+
+        public User logout()
+        {
+            return null;
         }
     }
 
--- project_B/Program.cs
+++ project_B/Program.cs
@@ -9,15 +9,65 @@ namespace Login
         public static void Main(string[] args)
         {
             Users accounts = new Users();
-            Console.WriteLine(accounts.users);
-            User MainUser = accounts.Login(accounts);
-            if (MainUser == null)
+            User currentUser = new User();
+            Console.WriteLine("Welcome\nWhat would you like to do:\n[1] Log In\n[2] Sign Up");
+            string ans = Console.ReadLine();
+            if(ans == "1")
             {
-                Console.WriteLine("Try again later");
+                Console.WriteLine("Enter email: ");
+                string mail = Console.ReadLine();
+                Console.WriteLine("Enter password: ");
+                string pass = Console.ReadLine();
+                currentUser = accounts.Login(accounts, mail, pass);
+            }
+            else if(ans == "2")
+            {
+                Console.WriteLine("Enter email: ");
+                string mail = Console.ReadLine();
+                Console.WriteLine("Enter password: ");
+                string pass = Console.ReadLine();
+                currentUser = accounts.Create(mail, pass, accounts);
+                if (currentUser == null)
+                {
+                    Console.WriteLine("Invalid email or email is already in use");
+                }
             }
             else
             {
-                Console.WriteLine($"Welcome {MainUser.UserName}, what would you like to do today?");
+                Console.WriteLine("Invalid character");
+            }
+
+            Console.WriteLine($"Hello {currentUser.UserName}, what would you like to do today?");
+            Console.WriteLine("[1] Book Flight\n[2] Check Flights\n[3] Log Out\n[4] Delete Account");
+            string ans2 = Console.ReadLine();
+            if(ans2 == "1")
+            {
+                Console.WriteLine();
+            }
+            if (ans2 == "2")
+            {
+                Console.WriteLine();
+            }
+            if (ans2 == "3")
+            {
+                currentUser = accounts.logout();
+            }
+            if (ans2 == "4")
+            {
+                Console.WriteLine("Are you sure you want to remove your account? [y/n]");
+                string ans3 = Console.ReadLine();
+                if (ans3 == "y")
+                {
+                    accounts.remove(currentUser, accounts);
+                }
+                else if (ans3 == "n")
+                {
+                    Console.WriteLine("Bruh");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Character");
+                }
             }
         }
 
