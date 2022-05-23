79d5afa8f6c409b83396d77621ff09d38ddc375c
miknijssen1002760 <1002760@hr.nl>
2022-05-23 12:54:36 +0200

    .

--- ConsoleApp1/Models/Flight.cs
+++ ConsoleApp1/Models/Flight.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleApp1.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,25 +7,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Controllers
 {
-    public class Flight
+    internal class Flight
     {
-        [JsonPropertyName("rij")]
-        public int Rij { get; set; }
+        [JsonPropertyName("StandaartPrijs")]
+        public int StandaartPrijs { get; set; }
 
-        [JsonPropertyName("datum")]
-        public string Datum { get; set; }
+        [JsonPropertyName("boekingsKosten")]
+        public int BoekingsKosten { get; set; }
 
-        [JsonPropertyName("dag")]
-        public string Dag { get; set; }
+        [JsonPropertyName("luchthavenBelasting")]
+        public int LuchthavenBelasting { get; set; }
 
-        [JsonPropertyName("vluchtNum")]
-        public string VluchtNum { get; set; }
+        [JsonPropertyName("eersteKlas")]
+        public int EersteKlas { get; set; }
+
+        [JsonPropertyName("toeslagStoelen")]
+        public int ToeslagStoelen { get; set; }
+
+        [JsonPropertyName("extraBaggage")]
+        public int ExtraBaggage { get; set; }
 
         [JsonPropertyName("duration")]
         public int Duration { get; set; }
-
-        [JsonPropertyName("uren")]
-        public List<Uur> Uren { get; set; }
 
 
     }
