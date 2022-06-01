88da6054ada666d9cfe9d96506ce0834e48a6d8e
Sem Hirschbein <1030180@hr.nl>
2022-06-01 15:11:46 +0200

    yes

--- project_B/Models/User.cs
+++ project_B/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using project_B.Models;
 
@@ -7,11 +8,17 @@ namespace project_B.Models
 {
     public class User
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; }
+        [JsonPropertyName("birthdate")]
+        public DateTime Birthday { get; set; }
         [JsonPropertyName("username")]
         public string UserName { get; set; }
         [JsonPropertyName("password")]
         public string Password { get; set; }
+        [JsonPropertyName("phonenumber")]
+        public string PhoneNumber { get; set; };
     }
 }
--- project_B/Views/AccountSettings.cs
+++ project_B/Views/AccountSettings.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using project_B.Models;
+using project_B.Controllers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,5 +30,26 @@ namespace project_B.Views
                     break;
             }
         }
+
+        public static void DeleteAccount()
+        {
+            Users accounts = new Users();
+            string[] options = { "Yes", "No" };
+            int CurrentSelection = MenuCreator.MultipleChoice(true, "Do you want to delete your account?", options);
+
+            switch (CurrentSelection)
+            {
+                case 0:
+                    accounts.remove(Login.currentUser, accounts);
+                    project_B.HomeScreen();
+                    break;
+
+                case 1:
+                    project_B.MainMenu();
+                    break;
+            }
+
+        }
+
     }
 }
