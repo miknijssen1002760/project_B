f3a861e02b12c63e8f5a27348b37dc46aa86cab8
miknijssen1002760 <1002760@hr.nl>
2022-06-07 14:08:30 +0200

    tests aangemaakt

---
+++ project_BTests1/Controllers/FlightsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using project_B.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_B.Controllers.Tests
+{
+    [TestClass()]
+    public class FlightsTests
+    {
+        [TestMethod()]
+        public void GetFlightsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void listAllTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
