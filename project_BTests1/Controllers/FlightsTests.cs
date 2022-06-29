95941e07e80900998d6b1e05645fbd0dfb4e7ddf
JesseVDHulle <jessevanderhulle@gmail.com>
2022-06-29 14:45:14 +0200

    test

---
+++ project_BTests1/Controllers/FlightsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using project_B.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_B.Controllers.Tests
+{
+    [TestClass()]
+    public class FlightsTests
+    {
+        Flights flights = new Flights();
+        [TestMethod()]
+        public void getLastIDTest()
+        {
+            Assert.AreEqual(flights.getLastID(), 4);
+        }
+        
+    }
+}
---
+++ project_BTests1/Views/AdminControlTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using project_B.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_B.Views.Tests
+{
+    [TestClass()]
+    public class AdminControlTests
+    {
+        [TestMethod()]
+        public void chooseOptionTest()
+        {
+            Assert.AreEqual(AdminControl.chooseOption(), "3");
+        }
+    }
+}
